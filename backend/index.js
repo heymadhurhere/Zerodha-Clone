13fbda3346fbbb30b7be7c0984072150dc02385c
Madhur Anand <anandmadhurrkm7@gmail.com>
2025-07-21 23:44:25 +0530

    link

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://zerodha-clone-k15n.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
